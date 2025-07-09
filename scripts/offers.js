b28916b1f99fea1652ae164cc8d090307baf90cf
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-07-09 14:40:51 +0200

    added a lot offers, pizza, pasta, salad, desserts and drinks as json

---
+++ scripts/offers.js
@@ -0,0 +1,164 @@
+let offers = [
+    {
+      "name": "Gemischter Salat",
+      "description": "Blattsalat, Tomaten, Gurken, Paprika, Karotten, Balsamico-Dressing",
+      "price": 4.50,
+      "type": "Salat"
+    },
+    {
+      "name": "Mediterraner Salat",
+      "description": "Rucola, Oliven, getrocknete Tomaten, Artischocken, Zitronen-Olivenöl-Dressing",
+      "price": 6.20,
+      "type": "Salat"
+    },
+    {
+      "name": "Kichererbsen-Salat",
+      "description": "Kichererbsen, Gurken, Tomaten, Petersilie, Minze, Zitronen-Dressing",
+      "price": 5.80,
+      "type": "Salat"
+    },
+    {
+      "name": "Linsensalat",
+      "description": "Belugalinsen, Rucola, Paprika, Frühlingszwiebeln, Senf-Vinaigrette",
+      "price": 6.00,
+      "type": "Salat"
+    },
+    {
+      "name": "Asia-Salat",
+      "description": "Chinakohl, Karotten, Edamame, Sesam, Soja-Ingwer-Dressing",
+      "price": 6.20,
+      "type": "Salat"
+    },
+    {
+      "name": "Avocado-Salat",
+      "description": "Blattsalat, Avocado, Cherry-Tomaten, Gurke, Limetten-Dressing",
+      "price": 6.90,
+      "type": "Salat"
+    },
+    {
+      "name": "Pizza Magherita",
+      "description": "Tomatensoße, veganer Pizzaschmelz, Basilikum",
+      "type": "Pizza",
+      "price": 8.50,  
+    },
+    {
+      "name": "Pizza Funghi",
+      "description": "Tomatensauce, veganer Pizzaschmelz, Champignons",
+      "price": 9.00,
+      "type": "Pizza"
+    },
+    {
+      "name": "Pizza Verdura",
+      "description": "Tomatensauce, veganer Pizzaschmelz, gegrilltes Gemüse, Rucola",
+      "price": 9.50,
+      "type": "Pizza"
+    },
+    {
+      "name": "Pizza Spinat",
+      "description": "Tomatensauce, veganer Pizzaschmelz, Spinat, Knoblauch",
+      "price": 9.20,
+      "type": "Pizza"
+    },
+    {
+      "name": "Pizza BBQ Jackfruit",
+      "description": "BBQ-Sauce, Jackfruit, Zwiebeln, veganer Pizzaschmelz",
+      "price": 10.00,
+      "type": "Pizza"
+    },
+    {
+      "name": "Spaghetti Pomodoro",
+      "description": "Spaghetti mit Tomatensauce und frischem Basilikum",
+      "price": 7.50,
+      "type": "Pasta"
+    },
+    {
+      "name": "Spaghetti Bolognese",
+      "description": "Spaghetti mit veganer Bolognese aus Sojahack",
+      "price": 8.50,
+      "type": "Pasta"
+    },
+    {
+      "name": "Linguuine Pesto",
+      "description": "Linguine mit hausgemachtem veganem Basilikumpesto und gerösteten Pinienkernen",
+      "price": 8.20,
+      "type": "Pasta"
+    },
+    {
+      "name": "Nudeln Funghi",
+      "description": "Bandnudeln mit veganer Pilzrahmsauce",
+      "price": 9.00,
+      "type": "Pasta"
+    },
+    {
+      "name": "Penne Arrabbiata",
+      "description": "Penne in scharfer Tomaten-Knoblauchsauce",
+      "price": 7.80,
+      "type": "Pasta"
+    },
+    {
+      "name": "Pasta Gemüse",
+      "description": "Fussilli mit gegrilltem Gemüse in Tomatensauce",
+      "price": 8.90,
+      "type": "Pasta"
+    },
+    {
+      "name": "Schoko-Mousse",
+      "description": "Cremige Mousse aus Avocado, Kakao und Ahornsirup",
+      "price": 4.50,
+      "type": "Dessert"
+    },
+    {
+      "name": "Chia-Pudding",
+      "description": "Chiasamen mit Kokosmilch getoppt Mangopüree",
+      "price": 4.20,
+      "type": "Dessert"
+    },
+    {
+      "name": "Veganes Tiramisu",
+      "description": "Mit Espresso getränkter Biskuit, Cashewcreme, Kakaopulver",
+      "price": 4.90,
+      "type": "Dessert"
+    },
+    {
+      "name": "Obstsalat",
+      "description": "Frisches Obst der Region mit Minze",
+      "price": 3.80,
+      "type": "Dessert"
+    },
+    {
+      "name": "Cola",
+      "description": "Erfrischendes koffeinhaltiges Getränk",
+      "price": 2.50,
+      "type": "Getränk"
+    },
+    {
+      "name": "Cola Zero",
+      "description": "Koffeinhaltige Cola ohne Zucker",
+      "price": 2.50,
+      "type": "Getränk"
+    },
+    {
+      "name": "Zitronenlimonade",
+      "description": "Hausgemachte Limonade mit frischer Zitrone",
+      "price": 2.80,
+      "type": "Getränk"
+    },
+    {
+      "name": "Eistee Pfirsich",
+      "description": "Gekühlter schwarzer Tee mit Pfirsichgeschmack",
+      "price": 2.70,
+      "type": "Getränk"
+    },
+    {
+      "name": "Mineralwasser",
+      "description": "Still oder Medium",
+      "price": 2.00,
+      "type": "Getränk"
+    },
+    {
+      "name": "Apfelschorle",
+      "description": "Erfrischende Mischung aus Apfelsaft und Mineralwasser",
+      "price": 2.60,
+      "type": "Getränk"
+    }
+]
