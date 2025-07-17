21ee7eb72fdc4a02da4b3a247dc1611cbe8bb55d
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-07-17 16:09:35 +0200

    changed function renderContent, added function renderOffers

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,47 @@
 
+let i = 0;
+
 function init(){
     //getFromLocalStorage();
+    renderContent();
     renderOffers();
 }
 
 // offers anzeigen
 
-function renderOffers(){
-    let i = 0;
+function renderContent(){
     let contentRef = document.getElementById("renderContent");
     contentRef.innerHTML = "";
     contentRef.innerHTML += `<h2>${offers[i].type}</h2>
                                 <div class="left">
                                     <div id="menu${i}" class="menu"></div>
                                 </div>
+                                <div class="right">
+                                    <div class="shopping_cart">
+                                        <h3>Dein Warenkorb</h3>
+                                            <div class="basket_content">
+                                                <p>Name</p>
+                                                <p>price</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 `
+};
+function renderOffers(){
+    let offersRef = document.getElementById(`menu${i}`);
+    offersRef.innerHTML = "";
 
     for (let i = 0; i < offers.length; i++) {
-        contentRef.innerHTML += `
-                                    <div id="name${i}">
-                                        <h3>Name</h3>
-                                        <p>description</p>
-                                        <p class="price">price</p> 
-                                    </div>
-                                    <div id="plus${i}" class="add">
-                                        <p>+</p>
-                                    </div>`
+        offersRef.innerHTML += `
+                                <div id="name${i}">
+                                    <h3>Name</h3>
+                                    <p>description</p>
+                                    <p class="price">price</p> 
+                                </div>
+                                <div id="plus${i}" class="add">
+                                    <p>+</p>
+                                </div>`
 }
 };
 
