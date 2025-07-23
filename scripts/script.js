37fc7e5b182c609dab9cd9472ea84e97adf3f4bf
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-07-23 14:35:19 +0200

    added function renderSingleMenu

--- scripts/script.js
+++ scripts/script.js
@@ -12,36 +12,48 @@ function init(){
 function renderContent(){
     let contentRef = document.getElementById("renderContent");
     contentRef.innerHTML = "";
-    contentRef.innerHTML += `   <div id="left${i}" class="left">
-                                      
-                                </div>
-                                <div class="right">
-                                    <div class="shopping_cart">
-                                        <h3>Dein Warenkorb</h3>
-                                            <div class="basket_content">
-                                                <p>Name</p>
-                                                <p>price</p>
-                                            </div>
+    contentRef.innerHTML += `<div id="left${i}" class="left">
+                            </div>
+                            <div class="right">
+                                <div class="shopping_cart">
+                                    <h3>Dein Warenkorb</h3>
+                                        <div class="basket_content">
+                                            <p>Name</p>
+                                            <p>price</p>
                                         </div>
                                     </div>
-                                </div>`
+                                </div>
+                            </div>`
 };
+
 function renderOffers(){
     let offersRef = document.getElementById(`left${i}`);
 
     for (let i = 0; i < offers.length; i++) {
-        offersRef.innerHTML += `<h2>${offers[i].type}</h2>
-                                <div id="menu${i}" class="menu">
-                                    <div id="name${i}">
-                                        <h3>name</h3>
-                                        <p>description</p>
-                                        <p class="price">price€</p>
-                                    </div>
-                                    <div id="plus${i}" class="add">
-                                        <p>+</p>
-                                    </div>
-                                </div>`
-}
+        offersRef.innerHTML += `<h2>${offers[i].type}</h2>`
+        renderSingleMenu(i);                            
+    }
+};
+
+function renderSingleMenu(i){
+    let singleMenuRef = document.getElementById(`left${i}`);
+    let info = offers[i].info;
+
+    for (let j = 0; j < info.length; j++) {
+        singleMenuRef.innerHTML += `<div id="name${i}">
+                                        <div id="menu${i}" class="menu">
+                                            <div class"info">
+                                                <h3>${info[j].name}</h3>
+                                                <p>${info[j].description}</p>
+                                                <p class="price">${info[j].price.toFixed(2)}€</p>
+                                            </div>
+                                            <div id="plus${i}" class="add">
+                                                <p>+</p>
+                                            </div>
+                                        </div>
+                                    </div>`
+    }
+
 };
 
 // add to basket + button
