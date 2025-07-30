d39458e6ec1e5ab5574b5a46f44d3285e134c63d
Noah Schulz <noah.johannes.schulz97@gmx.de>
2025-07-30 16:21:34 +0200

    added img trash, styling, changed addToBasket, added deleteItem

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-let i = 0;
+let itemArray = [];
 
 function init(){
     //getFromLocalStorage();
@@ -54,24 +54,36 @@ function renderSingleMenu(i){
 function addToBasket(i, j) {
     let info = offers[i].info;
     let basketRef = document.getElementById("basket");
-    let uniqueId = `amount${i}${j}`;
-    
-    basketRef.innerHTML += `
-        <div class="basket_content">
-            <h4>${info[j].name}</h4>   
-        </div>
-        <div class="calculate_and_amount">
-            <p class="orange" onclick="minus('${uniqueId}')">-</p>
-            <p id="${uniqueId}" class="gray">1</p>
-            <p class="orange" onclick="plus('${uniqueId}', ${info[j].price})">+</p>
-            <p>${info[j].price.toFixed(2).replace('.', ',')}€</p>
+    let amountId = `amount${i}${j}`;
+    let amountElement = document.getElementById(amountId);
+    let itemId = `item${i}${j}`; // eindeutige ID fürs Löschen
+
+    if (!amountElement) {
+        basketRef.innerHTML += `
+        <div class="basket_item" id="${itemId}">
+            <div class="basket_content">
+                <h4>${info[j].name}</h4>
+                <img src="img/icons/trash.svg" alt="delete Item" onclick="deleteItem('${itemId}')"> 
+            </div>
+            <div class="calculate_and_amount">
+                <p id="minus" class="orange" onclick="minus('${amountId}')">-</p>
+                <p id="${amountId}" class="gray">1</p>
+                <p class="orange" onclick="plus('${amountId}', ${info[j].price})">+</p>
+                <p>${info[j].price.toFixed(2).replace('.', ',')}€</p>
+            </div>
         </div>`;
-};
+    } else {
+        amountElement.textContent++;
+    }
+}
 
 // calculate price
 
-function calculatePrice(){
-
+function deleteItem(id){
+    let element = document.getElementById(id);
+    if (element) {
+        element.remove();
+    }
 };
 
 function plus(id, price) {
